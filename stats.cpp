ef38400788df47923f9c56a4366b8cd300897886
Nakul Ramanathan <nakul16168@iiitd.ac.in>
2020-09-03 10:04:06 +0530

    Update 1 on Stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,18 @@
+  
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+#include <bits/stdc++.h>
+namespace Statistics {
+    
+    Stats ComputeStatistics(const std::vector<float>& floatVector) {
+        struct Stats statsObj = {NAN, NAN, NAN};
+        auto size_floatVector = floatVector.size();
+        if(size_floatVector == 0)
+        {
+            return statsObj;
+        }
+        statsObj.average = accumulate( floatVector.begin(), floatVector.end(), 0.0) / size_floatVector;
+        statsObj.max = *max_element(floatVector.begin(), floatVector.end());
+        statsObj.min = *min_element(floatVector.begin(), floatVector.end());
+        return statsObj;
+    }
 }
