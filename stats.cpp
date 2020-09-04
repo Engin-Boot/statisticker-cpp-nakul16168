3e2b44c3472f764b9e652d29d243e26dbfe7fcd4
Nakul Ramanathan <nakul16168@iiitd.ac.in>
2020-09-04 09:47:32 +0530

    Added NaN

--- stats.cpp
+++ stats.cpp
@@ -4,7 +4,7 @@
 namespace Statistics {
     
     Stats ComputeStatistics(const std::vector<float>& floatVector) {
-        struct Stats obj1 = {0.0f, 0.0f, 0.0f};
+        struct Stats obj1 = {NaN, NaN, NaN};
         bool size_zero = (floatVector.size() == 0) ;
         if(size_zero)
         {
